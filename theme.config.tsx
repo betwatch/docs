32d79a600054240266fd468da0a390a3ae7ffc05
Jamie Watts <jamie@wattsjs.com>
2023-04-17 21:27:59 +1000

    🔔🎾🌴

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,8 +10,8 @@ const config: DocsThemeConfig = {
     logo: <Logo />,
     head: (
         <>
-            <meta name="description" content="Betwatch Documentation" />
-            <meta name="og:title" content="Betwatch Documentation" />
+            <meta name="description" content="Guides and tutorials for the Betwatch odds comparison platform" />
+            <meta name="og:title" content="Betwatch Guide" />
             <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </>
