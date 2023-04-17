2ea87fd8a71539bb3799d27704dcb0f9a972ae44
Jamie Watts <jamie@wattsjs.com>
2023-04-17 22:17:43 +1000

    🏌🍣🏑

--- theme.config.tsx
+++ theme.config.tsx
@@ -26,6 +26,7 @@ const config: DocsThemeConfig = {
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@BetwatchHQ" />
             <meta name="twitter:creator" content="@BetwatchHQ" />
+            <meta name="twitter:title" content={frontMatter.title || 'Betwatch Guide'} />
             <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         </>
