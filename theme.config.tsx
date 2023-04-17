7f2cda8bfac06c1ad920b799bb775b124f1d6790
Jamie Watts <jamie@wattsjs.com>
2023-04-17 21:48:47 +1000

    🌮🐿🎠

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import Logo from './components/Logo'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
     useNextSeoProps() {
@@ -11,6 +13,7 @@ const config: DocsThemeConfig = {
             },
             openGraph: {
                 type: 'website',
+                description: 'Guides and tutorials for the Betwatch odds comparison platform',
                 images: [
                     {
                         url: '/og-image.png',
@@ -23,12 +26,21 @@ const config: DocsThemeConfig = {
         }
     },
     logo: <Logo />,
-    head: (
-        <>
-            <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    head: () => {
+        const { asPath, defaultLocale, locale } = useRouter()
+        const { frontMatter } = useConfig()
+        const url =
+            'https://guide.betwatch.com' +
+            (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+        return <>
+            <meta property="og:url" content={url} />
+            <meta property="og:title" content={frontMatter.title || 'Betwatch Guide'} />
+            <meta property="og:description" content={frontMatter.description || 'Guides and tutorials for the Betwatch odds comparison platform'} />
             <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         </>
-    ),
+    },
     project: {
         link: 'https://github.com/betwatch',
     },
