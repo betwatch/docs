29d8ee3c54c110d67d1c904dafa77e34a3f2c737
Jamie Watts <jamie@wattsjs.com>
2023-04-17 21:27:20 +1000

    🚃🔬💛

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,20 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import Logo from './components/Logo'
 
 const config: DocsThemeConfig = {
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Betwatch Guide'
+        }
+    },
     logo: <Logo />,
+    head: (
+        <>
+            <meta name="description" content="Betwatch Documentation" />
+            <meta name="og:title" content="Betwatch Documentation" />
+            <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        </>
+    ),
     project: {
         link: 'https://github.com/betwatch',
     },
@@ -11,7 +24,7 @@ const config: DocsThemeConfig = {
     },
     docsRepositoryBase: 'https://github.com/betwatch/docs',
     footer: {
-        text: '2023 © Betwatch Pty Ltd',
+        text: <span>{new Date().getFullYear()} © Betwatch Pty Ltd</span>,
     },
 }
 
