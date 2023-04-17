6f31543d4e5ec807d8e755583a99e327e537b749
Jamie Watts <jamie@wattsjs.com>
2023-04-17 21:34:07 +1000

    🐫🚂🐚

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,13 +4,27 @@ import Logo from './components/Logo'
 const config: DocsThemeConfig = {
     useNextSeoProps() {
         return {
-            titleTemplate: '%s – Betwatch Guide'
+            titleTemplate: '%s – Betwatch Guide',
+            description: 'Guides and tutorials for the Betwatch odds comparison platform',
+            twitter: {
+                handle: '@BetwatchHQ',
+            },
+            openGraph: {
+                type: 'website',
+                images: [
+                    {
+                        url: '/og-image.png',
+                        width: 1200,
+                        height: 630,
+                        alt: 'Betwatch',
+                    }
+                ]
+            }
         }
     },
     logo: <Logo />,
     head: (
         <>
-            <meta name="description" content="Guides and tutorials for the Betwatch odds comparison platform" />
             <meta name="og:title" content="Betwatch Guide" />
             <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
