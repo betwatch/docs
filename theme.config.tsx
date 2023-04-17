10643986ae9c7d5f0f63f0ae03e775f859b38e64
Jamie Watts <jamie@wattsjs.com>
2023-04-17 21:52:00 +1000

    😊🎭🐭🍹

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,21 +8,6 @@ const config: DocsThemeConfig = {
         return {
             titleTemplate: '%s – Betwatch Guide',
             description: 'Guides and tutorials for the Betwatch odds comparison platform',
-            twitter: {
-                handle: '@BetwatchHQ',
-            },
-            openGraph: {
-                type: 'website',
-                description: 'Guides and tutorials for the Betwatch odds comparison platform',
-                images: [
-                    {
-                        url: '/og-image.png',
-                        width: 1200,
-                        height: 630,
-                        alt: 'Betwatch',
-                    }
-                ]
-            }
         }
     },
     logo: <Logo />,
@@ -37,6 +22,7 @@ const config: DocsThemeConfig = {
             <meta property="og:url" content={url} />
             <meta property="og:title" content={frontMatter.title || 'Betwatch Guide'} />
             <meta property="og:description" content={frontMatter.description || 'Guides and tutorials for the Betwatch odds comparison platform'} />
+            <meta property="og:image" content={frontMatter.image || '/og-image.png'} />
             <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         </>
